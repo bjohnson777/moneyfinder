026799cfd6c08a84f4b8b9d018b1f474a161509c
Barbara Johnson <bjdesignsit@icloud.com>
2025-05-15 09:48:50 -0500

    Create next.config.js
    Added on Thu May 15th

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  ouput : 'export',
+  trailingSlash: true, //Optional: ensures URLs end with a slash
+  };
+
+module.exports = nextConfig;
